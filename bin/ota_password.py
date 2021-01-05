41a386781caaaf7cb0c9d53279b17e386f9e0ab5
stormmurdoc <murdoc@storm-clan.de>
2021-01-05 23:25:08 +0100

    removed unused files

--- bin/ota_password.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import os
-
-# setup OTA password
-out=subprocess.run(["pass", "arduino/ota", "/dev/null"], capture_output=True)
-ota=out.stdout.decode('UTF-8').splitlines()
-os.environ[ESP_OTA_PASSWORD] = ota[0]
